02f9bd235ec0cb7dd8613ad376089a707a0c31c9
JDev <webdevoleg@gmail.com>
2020-01-31 16:43:28 +0100

    init Commit and codewars tasks

---
+++ ConvertStringToCamelCase.java
@@ -0,0 +1,23 @@
+import java.nio.CharBuffer;
+import java.util.Arrays;
+import java.util.logging.Logger;
+
+/**
+ * @author JDev
+ */
+public class ConvertStringToCamelCase {
+    private static Logger logger = Logger.getLogger("com.jdev.codewars");
+
+    public static void main(String[] args) {
+        logger.info(toCamelCase("the-stealth-warrior"));
+        logger.info(toCamelCase("The_Stealth_Warrior"));
+    }
+
+    static String toCamelCase(String s) {
+        final String camel = "";
+        CharBuffer.wrap(s).chars().forEach(c -> {
+            if (c == '-' || c == '_') camel.se += c;
+        });
+        return "";
+    }
+}
