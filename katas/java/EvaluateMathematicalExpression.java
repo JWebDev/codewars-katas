6b061e3a539fbde86cec0b94c6ae18cbfa182099
JDev <webdevoleg@gmail.com>
2020-02-16 22:24:46 +0100

    working version of Mathematical expression

--- katas/java/EvaluateMathematicalExpression.java
+++ katas/java/EvaluateMathematicalExpression.java
@@ -1,8 +1,8 @@
 package katas.java;
 
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
-import javax.script.ScriptException;
+import java.util.List;
+import java.util.Stack;
+import java.util.stream.Collectors;
 
 /**
  * @author JDev
@@ -11,16 +11,78 @@ import javax.script.ScriptException;
  */
 public class EvaluateMathematicalExpression {
 
-    public static void main(String[] args) throws ScriptException {
-        System.out.println("Result = " + calculate("1+1"));
+    public static void main(String[] args) {
+        System.out.printf("%-30s %-10.4f\n", "-123 = ", calculate("-123"));
+        System.out.printf("%-30s %-10.4f\n", "2 /2+3 * 4.75- -6 = ", calculate("2 /2+3 * 4.75- -6"));
+        System.out.printf("%-30s %-10.4f\n", "2 / (2 + 3) * 4.33 - -6 = ", calculate("2 / (2 + 3) * 4.33 - -6"));
+        System.out.printf("%-30s %-10.4f\n", "2 /2+3 * 4.75- -6 = ", calculate("2 /2+3 * 4.75- -6"));
+        System.out.printf("%-30s %-10.4f\n", "12* 123 = ", calculate("12* 123"));
+        System.out.printf("%-30s %-10.4f\n", "3 + 4 * 2 / ( 1 - ((55)) ) = ", calculate("3 + 4 * 2 / ( 1 - ((55)) )"));
     }
 
-    private static double calculate(String expression) throws ScriptException {
-        ScriptEngineManager scriptEngineManager = new ScriptEngineManager();
-        ScriptEngine scriptEngine = scriptEngineManager.getEngineByName("JavaScript");
-        Object ob = scriptEngine.eval(expression);
+    private static double calculate(String expression) {
+        Stack<Double> vStack = new Stack<Double>();
+        Stack<Character> oStack = new Stack<Character>();
+        List<Character> chars = expression.chars().mapToObj(c -> Character.valueOf((char) c)).collect(Collectors.toList());
+        for (int i = 0; i < chars.size(); i++) {
+            if (chars.get(i) == ' ')
+                continue;
 
-        return ((Integer) ob).doubleValue();
+            if (isDigit(chars, i) || isNegativeNumber(chars, i)) {
+                String buffer = "";
+                while (i < chars.size() && (isDigit(chars, i) || isNegativeNumber(chars, i)))
+                    buffer += chars.get(i++);
+                vStack.push(Double.valueOf(buffer));
+                i--;
+            } else if (chars.get(i) == '(') {
+                oStack.push(chars.get(i));
+            } else if (chars.get(i) == ')') {
+                while (oStack.peek() != '(')
+                    vStack.push(applyOperation(oStack.pop(), vStack.pop(), vStack.pop()));
+                oStack.pop();
+            } else if (chars.get(i).toString().matches("[\\+\\-\\*\\/]")) {
+                if (!isNegativeNumber(chars, i)) {
+                    while (!oStack.empty() && hasPrecedence(chars.get(i), oStack.peek()))
+                        vStack.push(applyOperation(oStack.pop(), vStack.pop(), vStack.pop()));
+                    oStack.push(chars.get(i));
+                }
+            }
+        }
+        while (!oStack.empty())
+            vStack.push(applyOperation(oStack.pop(), vStack.pop(), vStack.pop()));
+
+        return vStack.pop().doubleValue();
+    }
+
+    private static boolean isDigit(List<Character> chars, int index) {
+        return Character.isDigit(chars.get(index)) || chars.get(index) == '.';
+    }
+
+    private static boolean isNegativeNumber(List<Character> chars, int index) {
+        return (index == 0 || chars.get(index - 1) == ' ') && chars.get(index) == '-' && Character.isDigit(chars.get(index + 1));
+    }
+
+    private static boolean hasPrecedence(char lOp, char rOp) {
+        if (rOp == '(' || rOp == ')')
+            return false;
+        if ((lOp == '*' || lOp == '/') && (rOp == '+' || rOp == '-'))
+            return false;
+        else
+            return true;
+    }
+
+    private static double applyOperation(Character o, double r, double l) {
+        switch (o) {
+            case '-':
+                return l - r;
+            case '+':
+                return l + r;
+            case '*':
+                return l * r;
+            case '/':
+                return l / r;
+        }
+        return 0;
     }
 
 }
