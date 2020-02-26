6d70796fcd1f1ccbcdc7adecdfe35508328b9c7a
JDev <webdevoleg@gmail.com>
2020-02-26 22:12:52 +0100

    Tracking pawn solution not finished

--- katas/java/TrackingPawns.java
+++ katas/java/TrackingPawns.java
@@ -1,19 +1,42 @@
 package katas.java;
 
+import java.util.Arrays;
+import java.util.Map;
+import java.util.regex.Pattern;
 import java.util.stream.IntStream;
 
 /**
  * @author JDev
- *
+ * <p>
  * Kata: https://www.codewars.com/kata/56b012bbee8829c4ea00002c
  */
 public class TrackingPawns {
 
+    private static String[][] board = new String[8][];
+    private static Map<String, Integer> indexMap = Map.of("a", 1, "b", 2, "c", 3, "d", 4, "e", 5, "f", 6, "g", 7, "h", 8);
+
     public static void main(String[] args) {
-        System.out.println(multiplies(10));
+        generateBoard();
+        System.out.println(movePawns(new String[]{"e3", "d6", "e4", "a6"}));
     }
 
-    private static int multiplies(int number) {
-        return IntStream.range(0, number).filter(n -> (n % 3 == 0 || n % 5 == 0)).sum();
+
+    public static String[][] movePawns(String[] moves) {
+        Pattern pattern = Pattern.compile("\b[a-h]{1}[1-8]{1}\b");
+        Arrays.stream(moves).forEach(m -> {
+            boolean b = pattern.matcher(m).find();
+            String[] split = m.split("");
+            int[][] index = new int[][]{{indexMap.get(split[0])}, {Integer.valueOf(split[1])}};
+
+        });
+        return new String[0][0];
     }
+
+    private static void generateBoard() {
+        IntStream.iterate(8, i -> i - 1).limit(8).forEach(n -> {
+            board[8 - n] = Arrays.stream("abcdefgh".split("")).map(x -> x + n).toArray(String[]::new);
+        });
+        System.out.println(board);
+    }
+
 }
