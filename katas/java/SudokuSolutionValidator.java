7a02e477a6795ff5bc367d636a23df63f4d0891b
JDev <webdevoleg@gmail.com>
2020-05-01 20:38:08 +0200

    add sudoku validator

--- katas/java/SudokuSolutionValidator.java
+++ katas/java/SudokuSolutionValidator.java
@@ -1,19 +1,42 @@
 package katas.java;
 
-import java.util.stream.IntStream;
-
 /**
  * @author JDev
- *
+ * <p>
  * Kata: https://www.codewars.com/kata/529bf0e9bdf7657179000008
  */
 public class SudokuSolutionValidator {
 
+    private static int[][] validSudoku = {
+            {5, 3, 4, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 9, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}
+    };
+
+    private static int[][] invalidSudoku = {
+            {5, 3, 8, 6, 7, 8, 9, 1, 2},
+            {6, 7, 2, 1, 9, 5, 3, 4, 8},
+            {1, 9, 8, 3, 4, 2, 5, 6, 7},
+            {8, 5, 1, 7, 6, 1, 4, 2, 3},
+            {4, 2, 6, 8, 5, 3, 7, 9, 1},
+            {7, 1, 3, 9, 2, 4, 8, 5, 6},
+            {9, 6, 1, 5, 3, 7, 2, 8, 4},
+            {2, 8, 7, 4, 1, 9, 6, 3, 5},
+            {3, 4, 5, 2, 8, 6, 1, 7, 9}
+    };
+
     public static void main(String[] args) {
-        System.out.println(multiplies(10));
+        System.out.println(check(validSudoku));
+        System.out.println(check(invalidSudoku));
     }
 
-    private static int multiplies(int number) {
-        return IntStream.range(0, number).filter(n -> (n % 3 == 0 || n % 5 == 0)).sum();
+    public static boolean check(int[][] sudoku) {
+        return true;
     }
 }
