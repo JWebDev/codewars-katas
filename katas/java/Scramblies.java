c474b7c51b1a09ddbe98e12d36eb3fe2e480c273
JDev <webdevoleg@gmail.com>
2020-05-02 20:55:53 +0200

    add scrambles

--- katas/java/Scramblies.java
+++ katas/java/Scramblies.java
@@ -1,19 +1,35 @@
 package katas.java;
 
-import java.util.stream.IntStream;
+import java.util.List;
 
 /**
  * @author JDev
- *
+ * <p>
  * Kata: https://www.codewars.com/kata/55c04b4cc56a697bb0000048
  */
 public class Scramblies {
 
     public static void main(String[] args) {
-        System.out.println(multiplies(10));
+        System.out.println(scramble("rkqodlw","world")); //true
+        System.out.println(scramble("cedewaraaossoqqyt","codewars")); //true
+        System.out.println(scramble("katas","steak")); //false
+        System.out.println(scramble("scriptjavx", "javascript")); //false
+        System.out.println(scramble("scriptingjava","javascript")); //true
+        System.out.println(scramble("scriptsjava","javascripts")); //true
+        System.out.println(scramble("javscripts","javascript")); //false
+        System.out.println(scramble("aabbcamaomsccdd","commas")); //true
+        System.out.println(scramble("commas","commas")); //true
+        System.out.println(scramble("sammoc","commas")); //true
     }
 
-    private static int multiplies(int number) {
-        return IntStream.range(0, number).filter(n -> (n % 3 == 0 || n % 5 == 0)).sum();
+    public static boolean scramble(String str1, String str2) {
+        for (String l : List.of(str2.split(""))) {
+            if (str1.contains(l)) {
+                str1=str1.replaceFirst(l, "");
+            } else {
+                return false;
+            }
+        }
+        return true;
     }
 }
